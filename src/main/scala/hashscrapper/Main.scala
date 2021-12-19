ada2d933e19fa434c0db4614c3d67efe6dee6219
hashcode <boniface@kabaso.com>
2021-12-19 21:16:00 +0200

    update

--- src/main/scala/hashscrapper/Main.scala
+++ src/main/scala/hashscrapper/Main.scala
@@ -7,8 +7,13 @@ import com.google.common.io.Resources
 object Main extends App {
 
   val url = "https://www.znbc.co.zm/news/be-prayerfulsiliya-urges-zambians/"
-  val rawHTML = Resources.toString(new URL(url), Charsets.UTF_8)
+  val url2 ="https://zambianobserver.com/munir-zulu-needs-to-sober-up-behave-kazabu/"
+  val rawHTML = Resources.toString(new URL(url2), Charsets.UTF_8)
 
   val article = HashScrapper.extract(rawHTML)
   println(article.get.cleanedText.get)
+  println(article.get.publishDate)
+  println(article.get.title)
+  println(article.get.openGraphData)
+  println(article.get.canonicalLink)
 }
